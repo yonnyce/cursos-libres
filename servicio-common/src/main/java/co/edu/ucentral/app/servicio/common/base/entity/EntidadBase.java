b199e9a7ff2221e41cb697816c918d9444665307
Jonatan Quiroz <yonnyce@gmail.com>
2020-10-09 18:02:28 -0500

    Agregada configuracion de base de datos y servicios, corriendo correctamente en postgresql

--- servicio-common/src/main/java/co/edu/ucentral/app/servicio/common/base/entity/EntidadBase.java
+++ servicio-common/src/main/java/co/edu/ucentral/app/servicio/common/base/entity/EntidadBase.java
@@ -6,11 +6,13 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+@MappedSuperclass
 public class EntidadBase {
 
 	@Id
--- servicio-cursos-libres/src/main/java/co/edu/ucentral/app/service/CursoServiceImpl.java
+++ servicio-cursos-libres/src/main/java/co/edu/ucentral/app/service/CursoServiceImpl.java
@@ -1,20 +1,23 @@
 package co.edu.ucentral.app.service;
 
+import org.springframework.stereotype.Service;
+
 import co.edu.ucentral.app.model.Curso;
 import co.edu.ucentral.app.repository.CursoRepository;
 import co.edu.ucentral.app.servicio.common.service.CommonServiceImpl;
 
+@Service
 public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CursoService {
 
 	@Override
 	protected void verifyUniqueEntity(Curso entity) {
-		//Verificar unico 
-		
+		// Verificar unico
+
 	}
 
 	@Override
 	protected Curso mapUpdateableFields(Curso newVersionEntity, Curso oldVersionEntity) {
-		
+
 		return null;
 	}
 
